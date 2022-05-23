cbce6368e53b92b303bc80231e173bf222806b15
Andrey Prokhorov <beta737@yandex.ru>
2022-05-23 18:51:41 +0300

    выполнил урок №2

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 'use strict';
 
 
-let title,
-    screen,
-    screenPrice,
-    rollback,
-    fullPrice,
-    adaptive;
+let title = 'The study JS',
+    screens = "Простые, Сложные, Интерактивные",
+    screenPrice = 50,
+    rollback = 100,
+    fullPrice = 600000,
+    adaptive = true;
 
 alert('Hello World!!!');
 
 console.log('Я изучаю JS');
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+console.log(screens.length);
+
+console.log('Стоимость верстки экранов ' + screenPrice + ' рублей/ долларов/гривен/юани');
+console.log('Стоимость разработки сайта ' + fullPrice + ' рублей/ долларов/гривен/юани');
+
+screens = screens.toLowerCase();
+console.log(screens.split(','));
+
+console.log('Вывести в консоль Процент отката посреднику за работу ' + (fullPrice * (rollback / 100)));
